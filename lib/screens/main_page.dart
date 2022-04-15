292416e3974424fade03aebc8c447886236a7f72
sukrutto <sdeshmukh@mt.iitr.ac.in>
2022-04-15 16:14:21 +0530

    Add Camera functionality

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,9 +1,46 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_field
 
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final _localVideoRenderer = RTCVideoRenderer();
+  void initRenderers() async {
+    await _localVideoRenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': {
+        'facingMode': 'user',
+      }
+    };
+    MediaStream mediaStream =
+        await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+    _localVideoRenderer.srcObject = mediaStream;
+  }
+
+  @override
+  void initState() {
+    initRenderers();
+    _getUserMedia();
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    await _localVideoRenderer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +51,14 @@ class MainPage extends StatelessWidget {
         backgroundColor: Colors.grey,
       ),
       body: SafeArea(
-          child: Column(
+          child: Stack(
         children: [
-          FloatingActionButton(
-            child: const Icon(
-              Icons.add,
-              color: Colors.black87,
-            ),
-            onPressed: () {
-              print("SDP created");
-            },
-            backgroundColor: Colors.grey,
-          )
+          Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: RTCVideoView(_localVideoRenderer))
         ],
       )),
     );
